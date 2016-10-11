6984f0d98e7db23ec91bcb9ba3a390b117056b3f
Leonid Fedotov <me@ileonidze.ml>
2016-10-11 15:41:15 +0300

    ConsoleAPIProto added

--- src/main/java/com/ileonidze/jchat/ConsoleAPITest.java
+++ src/main/java/com/ileonidze/jchat/ConsoleAPITest.java
@@ -1,8 +1,11 @@
 package com.ileonidze.jchat;
 
-class ConsoleAPITest extends ConsoleAPIEvalProto {
-    public ConsoleResponse proceed(String[] commandParts) {
+import org.apache.log4j.Logger;
 
+class ConsoleAPITest extends ConsoleAPIEvalProto {
+    private static final Logger console = Logger.getLogger(ConsoleAPI.class);
+    public ConsoleResponse proceed(String[] commandParts) {
+        console.info("Running selftest...");
         return null;
     }
 }
